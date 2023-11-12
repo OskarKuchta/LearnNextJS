52f3bb6b58b670b16b84817e7154b8d45e7e14c0
Oskar <oskarkuchta5@gmail.com>
2023-11-12 18:46:20 +0100

    Add dynamic metadata for subpages

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -2,13 +2,23 @@ import getUser from "@/lib/getUser";
 import getUserPost from "@/lib/getUserPost";
 import { Suspense } from "react";
 import Link from "next/link";
+import { Metadata } from "next";
 
 type Params = {
   params: {
     userId: string;
   };
 };
-
+export async function generateMetadata({
+  params: { userId },
+}: Params): Promise<Metadata> {
+  const userData: Promise<User> = getUser(userId);
+  const user = await userData;
+  return {
+    title: user.name,
+    description: `This is page about ${user.name}`,
+  };
+}
 export default async function UserPage({ params: { userId } }: Params) {
   const userData: Promise<User> = getUser(userId);
   const userPostData: Promise<Post> = getUserPost(userId);
